8d4e45f315327c90cbd972b1cbd8ef3a90f693fe
elatigh <elatigh94@gmail.com>
2017-12-19 12:34:40 +0400

    first commit

---
+++ SOMACOGIRWEBSITE/resources/views/layouts/master.blade.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    
+    <title>first</title>
+    
+    
+
+
+    <link type="text/css" rel="stylesheet" href="src/css/css-reset.css">
+	<link type="text/css" rel="stylesheet" href="src/css/bootstrap.min.css">
+	<link type="text/css" rel="stylesheet" href="src/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="src/css/animate.css">
+    <link type="text/css" rel="stylesheet" href="src/css/owl.carousel.min.css">
+    <link type="text/css" rel="stylesheet" href="src/css/owl.theme.default.min.css">
+	<link type="text/css" rel="stylesheet" href="src/css/main.css">
+	<link type="text/css" rel="stylesheet" href="src/assets/css/responsive.css">
+
+    
+
+   
+</head>
+<body>
+    @include('partials.header')
+    
+
+       <div class="container">
+          @yield('content')
+          
+       
+       </div>
+       @yield('extra')
+   <!-- @include('partials.clients') 
+
+    @include('partials.up-footer')-->
+
+    @include('partials.footer1')
+       
+    
+    
+ <script src="src/js/jquery-2.2.3.min.js"></script>
+<script src="src/js/bootstrap.min.js"></script>
+<script src="src/js/jquery.counterup.min.js"></script>
+<script src="src/js/owl.carousel.min.js"></script>
+<script src="src/js/waypoints.min.js"></script>
+<script src="src/js/wow.min.js"></script>
+<script src="src/js/ajax.js"></script>
+<script src="src/js/main.js"></script>
+</body>
+</html>
---
+++ SOMACOGIRWEBSITE/resources/views/partials/footer1.blade.php
@@ -0,0 +1,61 @@
+<footer id="footer1">
+    <div class="container"> 
+        <div class="container-fluid">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-6 col-md-3 compay">
+                    <h4 class="ftrhead arimo">Société</h4>
+                    <ul>
+                    <li><a href="#l" class="ftrlinks">Contact</a></li>
+                    <li><a href="android-app-development.html" class="ftrlinks">Qui sommes-nous</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-md-3 compay">
+                    <h4 class="ftrhead arimo">Catalogues</h4>
+                    <ul>
+                    <li><a href="#" class="ftrlinks">Electricité</a></li>
+                    <li><a href="#l" class="ftrlinks">Quincaillerie</a></li>
+                    <li><a href="#" class="ftrlinks">Outillage</a></li>
+                    <li><a href="#" class="ftrlinks">Huiles</a></li>
+                    <li><a href="#" class="ftrlinks">Pompes</a></li>
+                    <li><a href="#" class="ftrlinks">Aciers</a></li>
+                    </ul>
+                </div>
+                <div class="col-xs-6 col-md-3 compay">
+                    <h4 class="ftrhead arimo">Services</h4>
+                    <ul>
+                    <li><a href="android-app-development.html" class="ftrlinks">ServiCe one</a></li>
+                    <li><a href="android-app-development.html" class="ftrlinks">Service two</a></li>
+                    <li><a href="android-app-development.html" class="ftrlinks">Service three</a></li>
+                    </ul>
+                </div>  
+
+                <div class="col-xs-6 col-md-3 compay">
+                    <h4 class="ftrhead arimo">Suivre SOMACOGIR</h4>
+                    <ul>
+                        <li><a target="_blank" href="#" class="soclink"><span class="fa fa-facebook"></span></a></li>
+                        <li><a target="_blank" href="" class="soclink"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a target="_blank" href="#" class="soclink"><span class="fa fa-twitter"></span></a></li>
+                    
+                    </ul>
+                </div>
+
+            </div>             
+        </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center line">
+                <hr class="bottom-hr" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <p class="copie-right">© SOMACOGIR 2017. Tous droits réservés. </p>
+            </div>
+        </div>
+    </div>
+  </div>
+  
+
+</div>    
